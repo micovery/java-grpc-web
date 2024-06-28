69bbdaca5461b304d73fc8ab9f644dc924e8e3e0
Miguel Mendoza <miguelmendoza@google.com>
2024-06-27 20:57:53 -0700

    bug: remove extra d.ts

--- backend/client/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { DemoServiceClient } from "./generated/demo.client";
-import { GrpcWebOptions } from "@protobuf-ts/grpcweb-transport";
-export * from "./generated/demo";
-export * from "./generated/demo.client";
-export declare class DemoClient extends DemoServiceClient {
-    constructor(options: GrpcWebOptions);
-}
